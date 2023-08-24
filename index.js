e524249c8a030c32d468d39483fcb59ac9546725
Khoa Vo <khoavo@Khoas-iMac.local>
2023-08-24 09:53:39 +0700

    update proxy

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+
+const proxy = require("html2canvas-proxy");
+// var express = require("express");
+
+// const app = express();
+app.use("/", proxy());
+
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo!')
+// })
+app.listen(process.env.PORT || 3000);
